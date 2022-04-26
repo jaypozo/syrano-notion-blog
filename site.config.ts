715d8961370c55e391f3bfb4252a964f0d7a0cbb
Jay <jaypozo@gmail.com>
2022-04-26 12:20:30 -0700

    nav updates

--- site.config.ts
+++ site.config.ts
@@ -45,16 +45,16 @@ export default siteConfig({
 
   // whether to use the default notion navigation style or a custom one with links to
   // important pages
-  navigationStyle: 'default'
-  // navigationStyle: 'custom',
-  // navigationLinks: [
-  //   {
-  //     title: 'About',
-  //     pageId: 'f1199d37579b41cbabfc0b5174f4256a'
-  //   },
-  //   {
-  //     title: 'Contact',
-  //     pageId: '6a29ebcb935a4f0689fe661ab5f3b8d1'
-  //   }
-  // ]
+  // navigationStyle: 'default'
+  navigationStyle: 'custom',
+  navigationLinks: [
+    {
+      title: 'Try Syrano ❤️',
+      url: 'https://syrano.ai/long-profiles'
+    },
+    {
+      title: 'About',
+      pageId: '335920d4f14c4f75a004a4fdad726857'
+    }
+  ]
 })
